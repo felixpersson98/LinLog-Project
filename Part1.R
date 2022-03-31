b8509595cfd5dd673e8cf79a0bf46f142c0d0fcd
unknown <ca7470ol-s@student.lu.se>
2022-03-31 18:43:11 +0200

    Sista delen av part 1 och en till graf

--- Part1.R
+++ Part1.R
@@ -20,8 +20,8 @@ summary(newplasma.model1)
 newplasma.pred1 <- 
   cbind(newplasma, 
         fit = predict(newplasma.model1),
-        conf = predict(newplasma.model1, interval = "confidence"))
-        # pred = predict(newplasma.model1, interval = "prediction"))
+        conf = predict(newplasma.model1, interval = "confidence"),
+        pred = predict(newplasma.model1, interval = "prediction"))
 head(newplasma.pred1)
 # get rid of the extra fits
 newplasma.pred1$conf.fit <- newplasma.pred1$pred.fit <- NULL
@@ -45,6 +45,7 @@ head(newplasma.pred1)
     labs(caption = "data and fitted line")
 )
 
+
 ###### Logarithmic model ######
 newplasma.model2 <- lm(log(betaplasma) ~ I(age - minage), data = newplasma)
 
@@ -52,11 +53,11 @@ newplasma.model2 <- lm(log(betaplasma) ~ I(age - minage), data = newplasma)
 newplasma.pred2 <- 
   cbind(newplasma, 
         fit = predict(newplasma.model2),
-        conf = predict(newplasma.model2, interval = "confidence"))
-        # pred = predict(newplasma.model2, interval = "prediction"))
+        conf = predict(newplasma.model2, interval = "confidence"),
+        pred = predict(newplasma.model2, interval = "prediction"))
 head(newplasma.pred2)
 # get rid of the extra fits
-# newplasma.pred1$conf.fit <- newplasma.pred2$pred.fit <- NULL
+newplasma.pred1$conf.fit <- newplasma.pred2$pred.fit <- NULL
 head(newplasma.pred2)
 
 # Plot log(beta-carotene) against age
@@ -75,6 +76,21 @@ head(newplasma.pred2)
     geom_line(aes(y = fit), color = "blue", size = 1) +
     labs(caption = "data and fitted line")
 )
+
+# Add confidence interval
+(
+  plot.conf2 <- plot.line2 + 
+    geom_ribbon(aes(ymin = conf.lwr, ymax = conf.upr), alpha = 0.2) +
+    labs(caption = "data, fitted line and 95% confidence interval")
+)
+
+# Add prediction interval
+plot.conf2 +
+  geom_line(aes(y = pred.lwr),
+            color = "red", linetype = "dashed", size = 1) +
+  geom_line(aes(y = pred.upr),
+            color = "red", linetype = "dashed", size = 1) +
+  labs(caption = "data, fitted line, 95% confidence and prediction intervals")
 
 
 ###### Residual Analysis ######
@@ -181,5 +197,43 @@ ggplot(data = newplasma.pred2, aes(x = e)) +
   labs(title = "Histogram of residuals") +
   theme(text = element_text(size = 14))
 
-## Choosing logarithmic model!
+### Choosing logarithmic model! ###
 
+# Calculate the confidence intervals for beta:
+conf <- confint(newplasma.model2)
+exp(conf)
+exp(newplasma.model2$coefficients)
+# confint(newplasma.model1)
+
+# Get confidence and prediction interval for x0 = 25
+
+(newplasma.x0 <- data.frame(age = c(25)))
+(newplasma.y0.pred2 <- cbind(newplasma.x0,
+                                 fit = exp(predict(newplasma.model2, newplasma.x0)),
+                                 conf = exp(predict(newplasma.model2, newplasma.x0, interval = "confidence")),
+                                 pred = exp(predict(newplasma.model2, newplasma.x0, interval = "prediction"))))
+(interval_width_25 <- newplasma.y0.pred2$conf.upr - newplasma.y0.pred2$conf.lwr)
+
+# Repeat for x0 = 26, 75 and 76
+
+newplasma.x0 <- data.frame(age = c(26))
+(newplasma.y0.pred2 <- cbind(newplasma.x0,
+                             fit = exp(predict(newplasma.model2, newplasma.x0)),
+                             conf = exp(predict(newplasma.model2, newplasma.x0, interval = "confidence")),
+                             pred = exp(predict(newplasma.model2, newplasma.x0, interval = "prediction"))))
+
+newplasma.x0 <- data.frame(age = c(75))
+(newplasma.y0.pred2 <- cbind(newplasma.x0,
+                             fit = exp(predict(newplasma.model2, newplasma.x0)),
+                             conf = exp(predict(newplasma.model2, newplasma.x0, interval = "confidence")),
+                             pred = exp(predict(newplasma.model2, newplasma.x0, interval = "prediction"))))
+(interval_width_75 <- newplasma.y0.pred2$conf.upr - newplasma.y0.pred2$conf.lwr)
+
+newplasma.x0 <- data.frame(age = c(76))
+(newplasma.y0.pred2 <- cbind(newplasma.x0,
+                             fit = exp(predict(newplasma.model2, newplasma.x0)),
+                             conf = exp(predict(newplasma.model2, newplasma.x0, interval = "confidence")),
+                             pred = exp(predict(newplasma.model2, newplasma.x0, interval = "prediction"))))
+
+120.4364 - 119.6004
+170.6154 - 169.431
