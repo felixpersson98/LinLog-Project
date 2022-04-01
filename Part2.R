93fa1c9eb21417d3eab7dd106004461157c1e902
unknown <ca7470ol-s@student.lu.se>
2022-04-01 14:04:33 +0200

    Fortsättning på part 2

--- Part2.R
+++ Part2.R
@@ -1,28 +1,39 @@
-# Import ggplot library
-library(ggplot2)
 
-# Read data file
-plasma <- read.delim("Data/plasma.txt")
 
 # turning the categorical variables into factors
-plasma$sex <- factor(plasma$sex,
+newplasma$sex <- factor(newplasma$sex,
                      levels = c(1, 2),
                      labels = c("Male", "Female"))
 
-plasma$smokstat <- factor(plasma$smokstat,
+newplasma$smokstat <- factor(newplasma$smokstat,
                      levels = c(1, 2, 3),
                      labels = c("Never", "Former", "Current Smoker"))
 
-plasma$bmicat <- factor(plasma$bmicat,
+newplasma$bmicat <- factor(newplasma$bmicat,
                      levels = c(1, 2, 3, 4),
                      labels = c("Underweight", "Normal", "Overweight", "Obese"))
 
-table(plasma$sex)
-table(plasma$smokstat)
-table(plasma$bmicat)
+table(newplasma$sex)
+table(newplasma$smokstat)
+table(newplasma$bmicat)
 
+# Create model and change reference region
+newplasma.model3 <- lm(log(betaplasma) ~ I(age - minage) + bmicat, data = newplasma)
 
+summary(newplasma.model3)
 
-plasma$region <- relevel(plasma$region, "all")
-plasma.model3 <- lm(log(Pb) ~ I(year - 1975) + region, data = plasma)
-(plasma.model3.sum <- summary(plasma.model3))
+# Change reference category
+newplasma$bmicat <- relevel(newplasma$bmicat, "Normal")
+newplasma.model3 <- lm(log(betaplasma) ~ I(age - minage) + bmicat, data = newplasma)
+summary(newplasma.model3)
+
+newplasma$sex <- relevel(newplasma$sex, "Female")
+
+# New model with all parameters
+
+newplasma.model4 <- lm(log(betaplasma) ~ I(age - minage) + sex + smokstat + bmicat, data = newplasma)
+exp(confint(newplasma.model4))
+(betas <- data.frame(beta=newplasma.model4$coefficients, 
+                     exp.beta=exp(newplasma.model4$coefficients),
+                     lower.boundary=exp(confint(newplasma.model4))[,1],
+                     upper.boundary=exp(confint(newplasma.model4)[,2])))
