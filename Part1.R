93b9be4e21b19eb79d594c59ef95c258ba7e6a13
Felix Persson <felix.persson@telia.com>
2022-04-05 11:47:22 +0200

    Renamed variables and went through code

--- Part1.R
+++ Part1.R
@@ -2,258 +2,192 @@
 library(ggplot2)
 
 # Read data file
-plasma <- read.delim("Data/plasma.txt")
-head(plasma)
-summary(plasma)
+data <- read.delim(file = "Data/plasma.txt")
+head(data)
+summary(data)
 
-# Delete data point with betaplasma=0
-newplasma <- plasma[plasma$betaplasma > 0, ]
+# Delete invalid datapoint and store minimum
+data <- data[data$betaplasma > 0, ]
+minage <- min(data$betaplasma)
 
-# Find minimum age
-minage <- min(newplasma$betaplasma)
 
 ###### Linear model ######
-newplasma.model1 <- lm(betaplasma ~ I(age - minage), data = newplasma)
-summary(newplasma.model1)
-confint(newplasma.model1)
+model1.linear <- lm(betaplasma ~ I(age - minage), data = data)
+summary(model1.linear)
+confint(model1.linear)
 
-# Fit a line
-newplasma.pred1 <- 
-  cbind(newplasma, 
-        fit = predict(newplasma.model1),
-        conf = predict(newplasma.model1, interval = "confidence"),
-        pred = predict(newplasma.model1, interval = "prediction"))
-head(newplasma.pred1)
+# Fit a line and display data
+model1.linear.pred <- cbind(data, 
+        fit = predict(model1.linear),
+        conf = predict(model1.linear, interval = "confidence"),
+        pred = predict(model1.linear, interval = "prediction"))
+head(model1.linear.pred)
 
-# get rid of the extra fits
-newplasma.pred1$conf.fit <- newplasma.pred1$pred.fit <- NULL
-head(newplasma.pred1)
+# Get rid of extra fits
+model1.linear.pred$conf.fit <- model1.linear.pred$pred.fit <- NULL
+head(model1.linear.pred)
 
-# Plot beta-carotene against age
+# Plot beta-carotene against age and save image
 (
-  plot.data1 <- 
-    ggplot(data = newplasma.pred1, aes(x = age, y = betaplasma)) + 
+  plot1.data <- 
+    ggplot(data = model1.linear.pred, aes(x = age, y = betaplasma)) + 
     geom_point(size = 2) +
     xlab("Age (years)") +
     ylab("Beta-carotene (ng/ml)") +
-    labs(title = "Age and plasma beta-carotene") +
+    labs(title = "Age and data beta-carotene") +
     theme(text = element_text(size = 12))
 )
 
 # Add the fitted line to the data plot
 (
-  plot.line1 <- plot.data1 + 
+  plot1.line <- plot1.data + 
     geom_line(aes(y = fit), color = "blue", size = 1) +
-    labs(title = "Age and plasma beta-carotene, linear model")
+    labs(title = "Age and data beta-carotene, linear model")
 )
 
 
 ###### Logarithmic model ######
-newplasma.model2 <- lm(log(betaplasma) ~ I(age - minage), data = newplasma)
+model2.log <- lm(log(betaplasma) ~ I(age - minage), data = data)
 
-# Fit a log line
-newplasma.pred2 <- 
-  cbind(newplasma, 
-        fit = predict(newplasma.model2),
-        conf = predict(newplasma.model2, interval = "confidence"),
-        pred = predict(newplasma.model2, interval = "prediction"))
-head(newplasma.pred2)
-# get rid of the extra fits
-newplasma.pred1$conf.fit <- newplasma.pred2$pred.fit <- NULL
-head(newplasma.pred2)
+# Fit a log line and display head
+model2.log.pred <- cbind(data, 
+        fit = predict(model2.log),
+        conf = predict(model2.log, interval = "confidence"),
+        pred = predict(model2.log, interval = "prediction"))
+head(model2.log.pred)
+
+# Get rid of the extra fits
+model2.log.pred$conf.fit <- model2.log.pred$pred.fit <- NULL
+head(model2.log.pred)
 
 # Plot log(beta-carotene) against age
 (
-  plot.data2 <- 
-    ggplot(data = newplasma.pred2, aes(x = age, y = log(betaplasma))) + 
+  plot2.data <- 
+    ggplot(data = model2.log.pred, aes(x = age, y = log(betaplasma))) + 
     geom_point(size = 2) +
     xlab("Age (years)") +
     ylab("log(Beta-carotene (ng/ml))") +
     labs(title = "Age and log(beta-carotene)") +
     theme(text = element_text(size = 12))
 )
+
 # Add the fitted line to the data plot
 (
-  plot.line2 <- plot.data2 + 
+  plot2.line <- plot2.data + 
     geom_line(aes(y = fit), color = "blue", size = 1) +
-    labs(title = "Age and plasma beta-carotene, logarithmic model")
+    labs(title = "Age and data beta-carotene, logarithmic model")
 )
 
 # Add confidence interval
 (
-  plot.conf2 <- plot.line2 + 
+  plot2.conf <- plot2.line + 
     geom_ribbon(aes(ymin = conf.lwr, ymax = conf.upr), alpha = 0.2)
 )
 
 # Add prediction interval
-plot.conf2 +
-  geom_line(aes(y = pred.lwr),
-            color = "red", linetype = "dashed", size = 1) +
-  geom_line(aes(y = pred.upr),
-            color = "red", linetype = "dashed", size = 1) +
-  labs(title = "Age and plasma beta-carotene, logarithmic model, with 
-       confidence and prediction intervals")
+plot2.conf + geom_line(aes(y = pred.lwr),
+                       color = "red", linetype = "dashed", size = 1) +
+             geom_line(aes(y = pred.upr), 
+                       color = "red", linetype = "dashed", size = 1) +
+             labs(title = "Age and data beta-carotene, logarithmic model, with 
+                       confidence and prediction intervals")
 
 
 ###### Residual Analysis ######
 ### Linear model ###
 
 # Add the residuals to the predicted data
-newplasma.pred1$e <- newplasma.model1$residuals
-head(newplasma.pred1)
+model1.linear.pred$e <- model1.linear$residuals
+head(model1.linear.pred)
 
 # Save the max-value in order to make the y-axis symmetrical 
 # in the plots.
-(max1.e <- max(abs(newplasma.pred1$e)))
-(newplasma.elims1 <- c(-max1.e, max1.e))
+(max1.e <- max(abs(model1.linear.pred$e)))
+(model1.linear.pred.elims <- c(-max1.e, max1.e))
 
 # Plot residuals against x, add a horizontal line at y=0,
 # and expand the y-axis to include +/- max residual.
-
-ggplot(data = newplasma.pred1, 
-       aes(x = age, y = e)) +
-  geom_point(size = 2) +
-  geom_hline(yintercept = 0) +
-  expand_limits(y = newplasma.elims1) +
-  xlab("År") +
-  ylab("Residual") +
-  labs(title = "Residuals vs x-values") +
-  theme(text = element_text(size = 14))
+ggplot(data = model1.linear.pred, aes(x = age, y = e)) + geom_point(size = 2) + 
+       geom_hline(yintercept = 0) + 
+       expand_limits(y = model1.linear.pred.elims) + xlab("År") + 
+       ylab("Residual") + labs(title = "Residuals vs x-values") +
+       theme(text = element_text(size = 14))
 
 # Plot residuals against yhat, add a horizontal line at y=0,
 # and expand the y-axis to include +/- max residual.
-
-ggplot(data = newplasma.pred1, aes(x = fit, y = e)) +
-  geom_point(size = 2) +
-  geom_hline(yintercept = 0) +
-  expand_limits(y = newplasma.elims1) +
-  xlab("Predicted betakaroten") +
-  ylab("Residual") +
-  labs(title = "Residuals vs predicted values Y-hat") +
-  theme(text = element_text(size = 14))
+ggplot(data = model1.linear.pred, aes(x = fit, y = e)) + geom_point(size = 2) +
+       geom_hline(yintercept = 0) + expand_limits(y = model1.linear.pred.elims) 
+        + xlab("Predicted betakaroten") + ylab("Residual") +
+       labs(title = "Residuals vs predicted values Y-hat") +
+       theme(text = element_text(size = 14))
 
 # Make a normal qq-plot of the residuals.
-ggplot(data = newplasma.pred1, aes(sample = e)) +
-  geom_qq(size = 2) +
-  geom_qq_line() +
-  labs(title = "Normal Q-Q-plot of the residuals") +
-  theme(text = element_text(size = 14))
+ggplot(data = model1.linear.pred, aes(sample = e)) + geom_qq(size = 2) +
+       geom_qq_line() + labs(title = "Normal Q-Q-plot of the residuals") +
+       theme(text = element_text(size = 14))
 
 # Histogram of the residuals:
-ggplot(data = newplasma.pred1, aes(x = e)) +
-  geom_histogram(bins = 10) +
-  xlab("Residuals") +
-  labs(title = "Histogram of residuals") +
-  theme(text = element_text(size = 14))
-
-
+ggplot(data = model1.linear.pred, aes(x = e)) +
+       geom_histogram(bins = 10) + xlab("Residuals") +
+       labs(title = "Histogram of residuals") +
+       theme(text = element_text(size = 14))
 
 
 ### Logarithmic model ###
 
 # Add the residuals to the predicted data
-newplasma.pred2$e <- newplasma.model2$residuals
-head(newplasma.pred2)
+model2.log.pred$e <- model2.log$residuals
+head(model2.log.pred)
 
 # Save the max-value in order to make the y-axis symmetrical 
 # in the plots.
-(max2.e <- max(abs(newplasma.pred2$e)))
-(newplasma.elims2 <- c(-max2.e, max2.e))
+(max2.e <- max(abs(model2.log.pred$e)))
+(model2.log.pred.elims <- c(-max2.e, max2.e))
 
 # Plot residuals against x, add a horizontal line at y=0,
 # and expand the y-axis to include +/- max residual.
 
-ggplot(data = newplasma.pred2, 
-       aes(x = age, y = e)) +
-  geom_point(size = 2) +
-  geom_hline(yintercept = 0) +
-  expand_limits(y = newplasma.elims2) +
-  xlab("År") +
-  ylab("Residual") +
-  labs(title = "Residuals vs x-values") +
-  theme(text = element_text(size = 14))
+ggplot(data = model2.log.pred, aes(x = age, y = e)) + geom_point(size = 2) +
+       geom_hline(yintercept = 0) + expand_limits(y = model2.log.pred.elims) +
+       xlab("År") + ylab("Residual") + labs(title = "Residuals vs x-values") +
+       theme(text = element_text(size = 14))
 
-# Plot residuals against yhat, add a horizontal line at y=0,
+# Plot residuals against y-hat, add a horizontal line at y=0,
 # and expand the y-axis to include +/- max residual.
-
-ggplot(data = newplasma.pred2, aes(x = fit, y = e)) +
-  geom_point(size = 2) +
-  geom_hline(yintercept = 0) +
-  expand_limits(y = newplasma.elims2) +
-  xlab("Predicted betakaroten") +
-  ylab("Residual") +
-  labs(title = "Residuals vs predicted values Y-hat") +
-  theme(text = element_text(size = 14))
+ggplot(data = model2.log.pred, aes(x = fit, y = e)) + geom_point(size = 2) +
+       geom_hline(yintercept = 0) + expand_limits(y = model2.log.pred.elims) +
+       xlab("Predicted betakaroten") + ylab("Residual") +
+       labs(title = "Residuals vs predicted values Y-hat") +
+       theme(text = element_text(size = 14))
 
 # Make a normal qq-plot of the residuals.
-ggplot(data = newplasma.pred2, aes(sample = e)) +
-  geom_qq(size = 2) +
-  geom_qq_line() +
-  labs(title = "Normal Q-Q-plot of the residuals") +
-  theme(text = element_text(size = 14))
+ggplot(data = model2.log.pred, aes(sample = e)) + geom_qq(size = 2) +
+       geom_qq_line() + labs(title = "Normal Q-Q-plot of the residuals") +
+       theme(text = element_text(size = 14))
 
 # Histogram of the residuals:
-ggplot(data = newplasma.pred2, aes(x = e)) +
-  geom_histogram(bins = 10) +
-  xlab("Residuals") +
-  labs(title = "Histogram of residuals") +
-  theme(text = element_text(size = 14))
+ggplot(data = model2.log.pred, aes(x = e)) + geom_histogram(bins = 10) +
+       xlab("Residuals") + labs(title = "Histogram of residuals") +
+       theme(text = element_text(size = 14))
+
 
 ### Choosing logarithmic model! ###
-
 # Calculate the confidence intervals for beta:
-(conf <- confint(newplasma.model2))
-newplasma.model2$coefficients
+(conf <- confint(model2.log))
+model2.log$coefficients
 exp(conf)
-exp(newplasma.model2$coefficients)
-# confint(newplasma.model1)
+exp(model2.log$coefficients)
 
-# Get confidence and prediction interval for x0 = 25
+# Predict for ages 25, 26, 75, 76
+(x0.ages <- data.frame(age = c(25, 26, 75, 76)))
+(x0.ages.pred <- cbind(x0.ages, 
+                        fit = exp(predict(model2.log, x0.ages)),
+                        conf = exp(predict(model2.log, x0.ages, 
+                                           interval = "confidence")),
+                        pred = exp(predict(model2.log, x0.ages, 
+                                           interval = "prediction"))))
 
-(newplasma.x0 <- data.frame(age = c(25)))
-(newplasma.y0.pred2 <- cbind(newplasma.x0,
-                                 fit = exp(predict(newplasma.model2, 
-                                                   newplasma.x0)),
-                                 conf = exp(predict(newplasma.model2, 
-                                                    newplasma.x0, 
-                                                    interval = "confidence")),
-                                 pred = exp(predict(newplasma.model2,
-                                                    newplasma.x0, 
-                                                    interval = "prediction"))))
-(interval_width_25 <- newplasma.y0.pred2$conf.upr - newplasma.y0.pred2$conf.lwr)
+x0.ages.pred$interval_width <- x0.ages.pred$conf.upr - x0.ages.pred$conf.lwr
 
-# Repeat for x0 = 26, 75 and 76
-
-newplasma.x0 <- data.frame(age = c(26))
-(newplasma.y0.pred2 <- cbind(newplasma.x0,
-                             fit = exp(predict(newplasma.model2, newplasma.x0)),
-                             conf = exp(predict(newplasma.model2,
-                                                newplasma.x0,
-                                                interval = "confidence")),
-                             pred = exp(predict(newplasma.model2,
-                                                newplasma.x0,
-                                                interval = "prediction"))))
-
-newplasma.x0 <- data.frame(age = c(75))
-(newplasma.y0.pred2 <- cbind(newplasma.x0,
-                             fit = exp(predict(newplasma.model2, newplasma.x0)),
-                             conf = exp(predict(newplasma.model2,
-                                                newplasma.x0,
-                                                interval = "confidence")),
-                             pred = exp(predict(newplasma.model2, 
-                                                newplasma.x0, 
-                                                interval = "prediction"))))
-(interval_width_75 <- newplasma.y0.pred2$conf.upr - newplasma.y0.pred2$conf.lwr)
-
-newplasma.x0 <- data.frame(age = c(76))
-(newplasma.y0.pred2 <- cbind(newplasma.x0,
-                             fit = exp(predict(newplasma.model2, newplasma.x0)),
-                             conf = exp(predict(newplasma.model2,
-                                                newplasma.x0,
-                                                interval = "confidence")),
-                             pred = exp(predict(newplasma.model2,
-                                                newplasma.x0,
-                                                interval = "prediction"))))
-
-120.4364 - 119.6004
-170.6154 - 169.431
+# Display interval ages and corresponding interval width
+cbind(x0.ages.pred["age"], x0.ages.pred["interval_width"]) 
